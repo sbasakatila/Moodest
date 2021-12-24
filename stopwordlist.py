ec66b6d3d091c46d585b625d155b6b8723540405
leylahayir <92671108+leylahayir@users.noreply.github.com>
2021-12-24 23:35:50 +0300

    Add files via upload
    stopwordlist and emoji dictionary

---
+++ stopwordlist.py
@@ -0,0 +1,14 @@
+emojis = {':)': 'smile', ':-)': 'smile', ';d': 'wink',';)':'wink',';D':'wink', ':-E': 'vampire', ':(': 'sad',
+          ':-(': 'sad', ':-<': 'sad', ':P': 'raspberry', ':O': 'surprised', ':-S':'confused' ,':S':'confused'
+          ':-@': 'shocked', ':@': 'shocked',':-$': 'confused', ':\\': 'annoyed',
+          ':#': 'mute', ':X': 'mute', ':^)': 'smile', ':-&': 'confused', '$_$': 'greedy',
+          '@@': 'eyeroll', ':-!': 'confused', ':-D': 'smile', ':-0': 'yell', 'O.o': 'confused',
+          '<(--)>': 'robot', 'd[--]b': 'dj', ":'-)": 'sadsmile', ';)': 'wink',
+          ';-)': 'wink', 'O:-)': 'angel','O*-)': 'angel', '=^.^=': 'cat'}
+
+## Defining set containing all stopwords in english.
+stopwordlist = [i,me,my,myself,we,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,
+herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,this,that,these,those,am,are,was,were,be,been,being,
+have,has,had,having,do,does,did,doing,a,an,the,but,because,until,of,at,by,about,against,between,into,through,during,before,after,
+above,below,to,up,down,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,
+most,other,some,such,no,nor,only,own,same,so,than,too,very,s,t,can,will,just,don,should,now]
